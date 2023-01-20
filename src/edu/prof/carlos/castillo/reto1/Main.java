8c75299d4a14d82d6a16048574b30ced7a8c6c78
ccastillo <v-ccastillo@vrbo.com>
2023-01-19 21:17:19 -0600

    changed modules for packages

---
+++ src/edu/prof/carlos/castillo/reto1/Main.java
@@ -0,0 +1,14 @@
+package edu.prof.carlos.castillo.reto1;
+/**
+ * Clase de lanzamiento de aplicación
+ *
+ * @author prof. carlos castillo
+ * */
+public class Main {
+    /**
+     * método main para reto 1, no contiene lógica de la solución
+     * */
+    public static void main(String[] args) {
+
+    }
+}

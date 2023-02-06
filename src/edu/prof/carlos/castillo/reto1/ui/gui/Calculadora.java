58b85026ca0b48b9c4a6cb1bdffa428e62c17e95
prof-carlos-castillo <carlos.castillo@tecmilenio.mx>
2023-02-06 17:15:49 -0600

    gui: agregando interfaz grafica a calculadora

---
+++ src/edu/prof/carlos/castillo/reto1/ui/gui/Calculadora.java
@@ -0,0 +1,13 @@
+package edu.prof.carlos.castillo.reto1.ui.gui;
+
+import javax.swing.*;
+
+public class Calculadora {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JTextField textField3;
+}

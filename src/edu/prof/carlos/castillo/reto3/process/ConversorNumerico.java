4c7f567f067dc281ca6a87efd258812e684d7f33
prof-carlos-castillo <carlos.castillo@tecmilenio.mx>
2023-02-07 09:24:34 -0600

    clase de conversor numerico

---
+++ src/edu/prof/carlos/castillo/reto3/process/ConversorNumerico.java
@@ -0,0 +1,17 @@
+package edu.prof.carlos.castillo.reto3.process;
+
+/**
+ * Esta clase contiene metodos para convertir numeros de una base numerica a otra
+ */
+public class ConversorNumerico {
+    /**
+     * Este metodo recibe un nstring que representa un numero en base decimal
+     * y regresa el mismo numero en su representacion de base binaria
+     *
+     * @param decimal numero a convertir en base decimal
+     * @return numero en base binaria
+     */
+    public static String convertirDecimalaBinario(String decimal) {
+
+    }
+}
